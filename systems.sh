dc33caddbd8b825f5f21bc40df7c9f89c8ab2673
EcclahNdege <bosiborindege18@gmail.com>
2024-07-26 11:05:26 +0300

    Added systems.sh

---
+++ systems.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create a directory called Lab1_Dir
+mkdir Lab1_Dir
+
+# Change the current directory to Lab1_Dir
+cd Lab1_Dir
+
+# Create an empty file named testfile.txt
+touch testfile.txt
+
+# Write "Hello, World!" into testfile.txt
+echo "Hello, World!" > testfile.txt
+
+# Display the contents of testfile.txt
+cat testfile.txt
+
+# List the files in Lab1_Dir
+ls
